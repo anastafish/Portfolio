dcb69045e4fc4d9880119a959bcd6c9921b0bf64
anastafish <anastafish24@gmail.com>
2023-02-13 15:34:11 +0300

    added new skill (tailwindcss)

--- src/components/skills.js
+++ src/components/skills.js
@@ -5,10 +5,12 @@ export default function Skills(props){
         <div className="skills-container reveal">
         <h1>Skills</h1>
         <div className="skills-icons">
-            <img className='img1' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt='JavaScript'/>
-            <img className='img2' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='React'/>
-            <img className='img3' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='Css3'/>
-            <img className='img4' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='html5'/>
+            <img title='javascript' className='img1' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt='JavaScript'/>
+            <img title='reactjs' className='img2' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='React'/>
+            <img title='css3' className='img3' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='Css3'/>
+            <img title="html5" className='img4' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='html5'/>            
+            <img title='tailwindcss' className='img5' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="tailwindcss"/>
+          
         </div>              
         </div>
     )
