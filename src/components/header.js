89222aef60d19f63b470d67af650b7b294920858
anastafish <anastafish24@gmail.com>
2023-03-29 17:42:37 +0300

    added links in the header section

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,28 @@
 import '../styles/header.css'
 
 export default function Header(){
+
+    function handleScroll(className){
+        const scrollDiv = document.querySelector(className).offsetTop - 100;
+        window.scrollTo({ top: scrollDiv, behavior: 'smooth'}); 
+    }
+
     return(
-        <div className="header-container">
+        <div className="header-container">            
              <div className='h1-container'>
                 <h1 className='header-h1'>Welcome To My Portfolio!</h1>
             </div>  
+            <div className='header-links'>
+                <h1 onClick={() => handleScroll(".projects")}>
+                    Projects
+                    </h1>
+                <h1 onClick={() => handleScroll(".skills-container")}>
+                    Skilss
+                    </h1>
+                <h1 onClick={() => handleScroll(".contact-container")}>
+                    Contact
+                    </h1>
+            </div>
         </div>
     )
 }
