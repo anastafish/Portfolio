75bed27eec665a3da03c8f4b0dcbafa4c16b09c4
anastafish <anastafish24@gmail.com>
2023-09-28 23:42:52 +0300

    removed the loading screen

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import './App.css';
 import Main from './components/main';
 import Header from './components/header';
 import ContactForm from './components/ContactForm';
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 import Footer from '../src/components/footer';
 
 function App() {
-  const [isLoading, setLoading] = useState(true);
+  // const [isLoading, setLoading] = useState(true);
 
-  function someRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
-    return new Promise(resolve => setTimeout(() => resolve(), 4500));
-  } 
+  // function someRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+  //   return new Promise(resolve => setTimeout(() => resolve(), 4500));
+  // } 
 
-  useEffect(() => {
-    someRequest().then(() => {
-      const loaderElement = document.querySelector(".loader-container");
-      if (loaderElement) {
-        loaderElement.remove();
-        setLoading(!isLoading);
-      }
-    });
-  });
+  // useEffect(() => {
+  //   someRequest().then(() => {
+  //     const loaderElement = document.querySelector(".loader-container");
+  //     if (loaderElement) {
+  //       loaderElement.remove();
+  //       setLoading(!isLoading);
+  //     }
+  //   });
+  // });
 
-  if (isLoading) {
-    return null;
-  }
+  // if (isLoading) {
+  //   return null;
+  // }
 
   return (
     <div className="App">
