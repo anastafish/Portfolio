638f6b7c248c62b80cdcde472744a38f0db5a5c7
anastafish <anastafish24@gmail.com>
2024-06-13 17:36:09 +0300

    removed part of the header

--- src/components/header.js
+++ src/components/header.js
@@ -12,17 +12,6 @@ export default function Header(){
              <div className='h1-container'>
                 <h1 className='header-h1'>Welcome To My Portfolio!</h1>
             </div>  
-            <div className='header-links'>
-                <h1 onClick={() => handleScroll(".projects", 70)}>
-                    Projects
-                    </h1>
-                <h1 onClick={() => handleScroll(".skills-container", 120)}>
-                    Skilss
-                    </h1>
-                <h1 onClick={() => handleScroll(".contact-container", 60)}>
-                    Contact
-                    </h1>
-            </div>
         </div>
     )
 }
